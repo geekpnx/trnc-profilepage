2d5f5c9e56e980da8464872f9142457e6939890c
Terence <terence7ds@gmail.com>
2024-01-09 16:39:29 +0100

    Modified form.html and added form.js file date 090124

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,13 +1,32 @@
 // alert("Yeah, it is working!")
 function validation() {
 
-    const email = document.registration.email.value
     const firstName = document.registration.first_name.value
     const lastName = document.registration.last_name.value
+    const gender = document.registration.gender.value
+    const email = document.registration.email.value
+    const country = document.registration.country.value
+
+  
+
+    if (firstName.length < 8) {
+        alert('First name shoud be at least 8 characters long')
+        return false
+    }
+
+    if (lastName.length == 0) {
+        alert ('Last name must be provided')
+        return false
+    }
 
     if (email.endsWith('@dci-student.org') == false) {
-    alert('Email should end with @dci-student.org')
-    return false
-    })
+        alert('Email should end with @dci-student.org')
+        return false
+        }
+    
+    if (country == 'us' && gender != 'female') {
+        alert ('We only register females in USA')
+        return false
+    }
 
 }
